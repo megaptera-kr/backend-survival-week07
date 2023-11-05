ef1c3ca9ca8a10691b7a82c3383c3805378e21f3
hoeseong <hoeseong19@naver.com>
2023-11-05 15:36:13 +0900

    feat: Post의 속성 변경을 위한 객체들 생성

---
+++ src/main/java/kr/megaptera/assignment/models/posts/Author.java
@@ -0,0 +1,37 @@
+package kr.megaptera.assignment.models.posts;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Author {
+    @Column(name = "author")
+    private String value;
+
+    private Author() {
+    }
+
+    public Author(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(value, author.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
---
+++ src/main/java/kr/megaptera/assignment/models/posts/Content.java
@@ -0,0 +1,37 @@
+package kr.megaptera.assignment.models.posts;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Content {
+    @Column(name = "content")
+    private String value;
+
+    private Content() {
+    }
+
+    public Content(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Content content = (Content) o;
+        return Objects.equals(value, content.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
